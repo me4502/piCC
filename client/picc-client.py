37adb0e68cb5e4eb25240231fc42667ca7913d1b
Me4502 <mnmiller1@hotmail.com>
2016-05-20 20:40:10 +1000

    Added the modules for picc-client

--- client/picc-client.py
+++ client/picc-client.py
@@ -1,2 +1,11 @@
 import picamera
+import gzip
+import configparser
+import os
+import uuid
+import json
+import base64
+import requests
 
+from Crypto.PublicKey import RSA
+from Crypto import Random
